5ebe90787d320263046a33cc30a036d65a0ab7fe
sem223@lehigh.edu <Sean#282828>
2022-09-14 19:20:18 -0400

    R script connection and code

---
+++ Assignment 2.R
@@ -0,0 +1,17 @@
+setwd("~/BIS 244/BIS-244-Fall-2022-mckennasean/covid-19-data")
+state <- read.csv("us-states.csv")
+View(state)
+library(tidyverse)
+state_PA <- filter(state, state=="Pennsylvania")
+View(state_PA)
+n <- length(state_PA$cases)
+state_PA$incr_cases <- 1
+state_PA$incr_deaths <- 1
+for (i in 2:n) {
+  state_PA$incr_cases[i] <- (state_PA$cases[i]-state_PA$case[i-1])
+}
+for (i in 2:n) {
+  state_PA$incr_deaths[i] <- state_PA$deaths[i]-state_PA$deaths[i-1]
+}
+view(state_PA)
+print(sd(state_PA$incr_cases))
